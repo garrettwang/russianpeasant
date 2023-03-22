0b56429c3c97ad4dc8eccba23d7f4f6f720c9638
garrettwang <habseligkeit@gmail.com>
2023-03-22 21:46:29 +0100

    initial commit

---
+++ src/main/java/ch/css/al1/russianpeasant/RussianpeasantMultiplicator.java
@@ -0,0 +1,7 @@
+package ch.css.al1.russianpeasant;
+
+public class RussianpeasantMultiplicator {
+    public int mul(int x, int y) {
+        return y;
+    }
+}
---
+++ src/test/java/ch/css/al1/russianpeasant/RussianpeasantMultiplicatorTest.java
@@ -0,0 +1,27 @@
+package ch.css.al1.russianpeasant;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+class RussianpeasantMultiplicatorTest {
+    public static Stream<Arguments> testdataprovider() {
+        return Stream.of(
+                Arguments.of(1,1,1),
+                Arguments.of(1,2,2),
+                Arguments.of(1,3,3)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("testdataprovider")
+    void testMultiplicator(int x, int y, int expected) {
+
+        RussianpeasantMultiplicator testee = new RussianpeasantMultiplicator();
+        int actual = testee.mul(x,y);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
